d3a3ff995c56de4ec4178fdb576402f3219e9db7
ginger <26461046+gingerchew@users.noreply.github.com>
2023-10-12 10:33:06 -0400

    Updates build process to match babystore

---
+++ build/index.js
@@ -0,0 +1,27 @@
+// @ts-check
+/// <reference path="./node_modules/esbuild/lib/main.d.ts">
+import esbuild from 'esbuild';
+
+/** @type {esbuild.BuildOptions} */
+const config = {
+    entryPoints: ['src/index.js'],
+    outfile: 'dist/index.js',
+    sourcemap: false,
+    target: ['es2020'],
+    format: 'esm',
+    logLevel: 'info',
+    bundle: true,
+};
+
+const minConfig = Object.assign({}, config, {
+    outfile: 'dist/index.min.js',
+    sourcemap: true,
+    minify: true,
+});
+
+try {    
+    esbuild.build(minConfig)
+    esbuild.build(config);
+} catch(err) {
+    throw err;
+}
