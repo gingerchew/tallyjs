f2915524f92f130b7227ec4b12326156da7aeadb
ginger <26461046+gingerchew@users.noreply.github.com>
2023-10-12 19:49:43 -0400

    Updates types
    now all types are optional (cause they are)
    
    adds better typing for formatterOptions
    
    includes some new planned features
    - customFormatter

--- types.d.ts
+++ types.d.ts
@@ -10,11 +10,25 @@
  * @prop {1|-1} dir
  * @returns 
  */
-interface TallyOptions {
-    end: number;
-    start: number;
-    decimals: number;
-    // formatter: ;
-    locale: string;
-    dir: 1|-1;
+export interface TallyOptions {
+    end?: number;
+    start?: number;
+    decimals?: number|false;
+    formatterOptions?: Intl.NumberFormatOptions; // should be the options for Intl.NumberFormat - locale
+    locale?: string;
+    dir?: 1|-1;
+    prefix?: string;
+    suffix?: string;
+    duration?: number;
+    customFormatter?: {
+        [index: string]: unknown;
+        format: (num: number) => string;
+    }
 }
+
+export interface TallyAPI {
+    count: (ts?:number) => void;
+    stop: () => void;
+}
+
+export type Tally = (el:HTMLElement, options: TallyOptions) => TallyAPI
